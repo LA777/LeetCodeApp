dba0641a169dce90e4318b0fb613edcd5e038893
Alex L <LA.xdna@gmail.com>
2024-12-02 22:19:57 -0800

    added Task 876

---
+++ LeetCodeDotNet/Tasks/Task876.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCodeDotNet.Tasks
+{
+    /// <summary>
+    /// https://leetcode.com/problems/middle-of-the-linked-list/description/
+    /// </summary>
+    public class Task876
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            int count = 0;
+            var middleNode = head;
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                count++;
+
+                if (count % 2 == 0)
+                {
+                    if (middleNode.next is not null)
+                    {
+                        middleNode = middleNode.next;
+                    }
+                }
+
+                currentNode = currentNode.next;
+            }
+
+            return middleNode;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ UnitTests/TasksTests/Task876Tests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using LeetCodeDotNet.Tasks;
+
+namespace UnitTests.TasksTests
+{
+    public class Task876Tests
+    {
+        private readonly Task876 _sut = new Task876();
+
+        [Theory]
+        [InlineData(new[] { 1 }, new[] { 1 })]
+        [InlineData(new[] { 1, 2 }, new[] { 2 })]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 2, 3 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 3, 4 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 3, 4, 5 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 4, 5, 6 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }, new[] { 9, 10, 11, 12, 13, 14, 15, 16 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 }, new[] { 9, 10, 11, 12, 13, 14, 15, 16, 17 })]
+        public void MiddleNode_Should_Work_As_Expected(int[] input, int[] output)
+        {
+            var listNodeHead = ConvertArrayToListNode(input);
+            var listNodeHeadOutput = ConvertArrayToListNode(output);
+
+            var actualOutput = _sut.MiddleNode(listNodeHead);
+
+            actualOutput.Should().BeEquivalentTo(listNodeHeadOutput);
+        }
+
+        private ListNode ConvertArrayToListNode(int[] array)
+        {
+            var listNodeHead = new ListNode(array[0]);
+            var lastElement = array[array.Length - 1];
+            var nextNode = new ListNode(lastElement);
+
+            for (int i = array.Length - 2; i >= 0; i--)
+            {
+                var value = array[i];
+                var listNode = new ListNode(value, nextNode);
+                nextNode = listNode;
+                listNodeHead = listNode;
+            }
+
+            return listNodeHead;
+        }
+    }
+}
