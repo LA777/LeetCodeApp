406ffbcfe6f1fd61325a8e0d1e416bfb90961bd9
Alex L <LA.xdna@gmail.com>
2024-12-02 23:12:29 -0800

    Added Task 1342

---
+++ LeetCodeDotNet/Tasks/Task1342.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeDotNet.Tasks
+{
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+    /// </summary>
+    public class Task1342
+    {
+        public int NumberOfSteps(int num)
+        {
+            return Recursion(num, 0);
+        }
+
+        private int Recursion(int number, int step)
+        {
+            if (number < 1)
+            {
+                return step;
+            }
+
+            if (number % 2 == 0)
+            {
+                number = number / 2;
+            }
+            else
+            {
+                number--;
+            }
+
+            step++;
+
+            return Recursion(number, step);
+        }
+    }
+}
---
+++ UnitTests/TasksTests/Task1342Tests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using LeetCodeDotNet.Tasks;
+
+namespace UnitTests.TasksTests
+{
+    public class Task1342Tests
+    {
+        private readonly Task1342 _sut = new Task1342();
+
+        [Theory]
+        [InlineData(14, 6)]
+        [InlineData(8, 4)]
+        [InlineData(123, 12)]
+        public void RunningSum_Should_Work_As_Expected(int input, int output)
+        {
+            var actualOutput = _sut.NumberOfSteps(input);
+
+            actualOutput.Should().Be(output);
+        }
+    }
+}
