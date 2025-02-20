2b7da4295a3a11af59b654ca6ef9a61bfb26cb52
Alex L <LA.xdna@gmail.com>
2025-02-19 23:42:43 -0800

    added TribonacciNumber1137

---
+++ LeetCodeDotNet/DynamicProgramming/TribonacciNumber1137.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeDotNet.DynamicProgramming;
+
+/// <summary>
+/// https://leetcode.com/problems/n-th-tribonacci-number/description
+/// </summary>
+public class TribonacciNumber1137
+{
+    public int Tribonacci(int n)
+    {
+        const int length = 3;
+
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        if (n <= 3)
+        {
+            return n - 1;
+        }
+
+        if (n > 37)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+
+        var array = new int[length] { 1, 1, 2 };
+        var result = 0;
+
+        for (var i = length; i < n; i++)
+        {
+            result = array.Sum();
+            array[i % length] = result;
+        }
+
+        return result;
+    }
+}
---
+++ UnitTests/DynamicProgramming/TribonacciNumber1137Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetCodeDotNet.DynamicProgramming;
+
+namespace UnitTests.DynamicProgramming;
+
+public class TribonacciNumber1137Tests
+{
+    private readonly TribonacciNumber1137 _sut = new ();
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(4, 4)]
+    [InlineData(25, 1389537)]
+
+    public void Fib_Should_Work_As_Expected(int input, int output)
+    {
+        var actualOutput = _sut.Tribonacci(input);
+
+        actualOutput.Should().Be(output);
+    }
+}
